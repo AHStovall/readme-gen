514e73d39a427089e5b2a7353bf481614b045e57
AHStovall <adamhstovall@gmail.com>
2023-02-24 18:31:51 -0500

    Bug fixes, refactor of file structure, generate Markdown and index.js. Improvements made to file structure, ready for final push and resubmit

---
+++ index.js
@@ -0,0 +1,98 @@
+// TODO: Include packages needed for this application
+
+const inquirer = require("inquirer");
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+const questions = [
+  // title - ask for project title
+  {
+    type: "input",
+    message: "What is your project title?",
+    name: "title",
+  },
+  // description - ask for description of project
+  {
+    type: "input",
+    message:
+      "Provide a short description explaining the what, why, and how of your project.",
+    name: "description",
+  },
+  // installation - ask for steps to install project
+  {
+    type: "input",
+    message:
+      "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+    name: "installation",
+  },
+  // usage - ask for instructions and examples for app use
+  {
+    type: "input",
+    message: "Provide instructions and examples for use.",
+    name: "usage",
+  },
+  // credits - list collaborators if any
+  {
+    type: "input",
+    message:
+      "List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section. If you followed tutorials, include links to those here as well.",
+    name: "credits",
+  },
+  // license should be in list form, no spaces
+  {
+    type: "list",
+    message: "Which license do you want to include?",
+    choices: ["Apache_2.0", "ISC", "MIT"],
+    name: "license",
+  },
+  // features - list features here
+  {
+    type: "input",
+    message: "If your project has a lot of features, list them here.",
+    name: "features",
+  },
+  // contribute - share how to contribute to this app
+  {
+    type: "input",
+    message:
+      "If you created an application or package and would like other developers to contribute to it, you can include guidelines for how to do so here.",
+    name: "contribute",
+  },
+  // tests - provide examples if tests written for this app, how to run them
+  {
+    type: "input",
+    message:
+      "If you've written tests for your application, provide examples on how to run them here.",
+    name: "tests",
+  },
+  // include the next two parts in Questions section
+  {
+    type: "input",
+    message: "What is your Github username?",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+];
+
+
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  inquirer
+  .prompt(questions)
+  .then((data) =>{
+    console.log(data);
+const filename = `${data.title.toLowerCase().split(' ').join("")}.md`;
+
+    fs.writeFile(filename, generateMarkdown(data), (err) =>
+    err ? console.log(err): console.log("Success!")
+    );
+})
+};
+
+writeToFile();
---
+++ readmegenerator.md
@@ -0,0 +1,58 @@
+## README Generator
+
+  ![Github license](https://img.shields.io/static/v1?label=License&message=MIT&color=brightgreen)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Features](#features)
+  - [How to Contribute](#how-to-contribute)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+## Description
+The motivation is to assist in developing well curated and professional READMEs to accompany assignments completed during this bootcamp. This will assist in developing said READMEs and keeping a consistent format across multiple assignments for documentation.
+
+## Installation
+Enter GitBash, type 'npm i' to ensure all packages are downloaded and then type 'node index.js' to start
+
+## Usage
+It solves a need for the consistency and ensures high quality READMEs being produced every time.
+
+## Credits
+N/A
+
+## Features
+N/A
+
+## Liscense
+Copyright (c) 2012-2022 
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+  
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+  
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+## Contribute
+N/A
+(https://www.contributor-covenant.org/)
+
+## Tests
+N/A
+
+## Questions
+https://github.com/AHStovall/
+adamhstovall@gmail.com
